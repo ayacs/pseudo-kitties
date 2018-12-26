b3465a57fb894e8f0dd6b37012a0b339ddcd35b1
Artem <ayacs87@gmail.com>
2018-12-26 13:58:45 +0200

    Updates

---
+++ precache-manifest.f67c3203d9b1285fc349812e4c9c0a66.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/pseudo-kitties/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "0b3a129684761df014b5",
+    "url": "/pseudo-kitties/static/css/main.60c4a565.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/pseudo-kitties/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "d41489c03e232760591e",
+    "url": "/pseudo-kitties/static/js/1.d41489c0.chunk.js"
+  },
+  {
+    "revision": "20986b00bd23cb8e3530",
+    "url": "/pseudo-kitties/static/js/runtime~main.20986b00.js"
+  },
+  {
+    "revision": "0b3a129684761df014b5",
+    "url": "/pseudo-kitties/static/js/main.0b3a1296.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/pseudo-kitties/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/pseudo-kitties/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/pseudo-kitties/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "d41489c03e232760591e",
+    "url": "/pseudo-kitties/static/css/1.b768348d.chunk.css"
+  },
+  {
+    "revision": "ce10b5cfe53fcf5533cd8704052ea59d",
+    "url": "/pseudo-kitties/index.html"
+  }
+];
